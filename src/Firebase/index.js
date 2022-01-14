81efa08a650138d25a9e65d8f9f614c129237990
Євген Євген <epronenko@gmail.com>
2022-01-14 13:09:48 +0200

    first commit

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -1,34 +1,21 @@
-import firebase from "firebase";
-import 'firebase/auth'
-import 'firebase/firestore'
-import 'firebase/storage'
-import 'firebase/database'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDIJ3aK1Ya_158T60Ggmx0i3qd-eFK29gw",
-    authDomain: "test-fbauth.firebaseapp.com",
-    projectId: "test-fbauth",
-    storageBucket: "test-fbauth.appspot.com",
-    messagingSenderId: "160699708538",
-    appId: "1:160699708538:web:430099ff5cce1b9016b7c3",
-    measurementId: "G-GB65DJNF6Q"
+    apiKey: "AIzaSyCd2gHVrVhKy0RHxAKZzTcLndd9uf0fcxo",
+    authDomain: "mytest-fb-auth.firebaseapp.com",
+    projectId: "mytest-fb-auth",
+    storageBucket: "mytest-fb-auth.appspot.com",
+    messagingSenderId: "754938370096",
+    appId: "1:754938370096:web:4d6254d2e47ed3236af5f5",
+    measurementId: "G-QZQWG4X682"
 };
 
-
-
-export const fire = firebase.initializeApp(firebaseConfig)
-
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-/*
-// Allow read/write access to all users under any conditions
-// Warning: **NEVER** use this rule set in production; it allows
-// anyone to overwrite your entire database.
-service cloud.firestore {
-    match /databases/{database}/documents {
-    match /{document=**} {
-        allow read, write: if true;
-    }
-}
-}*/
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
