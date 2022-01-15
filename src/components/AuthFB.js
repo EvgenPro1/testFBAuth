a46bfef909d36050873198a20a93656cd0987f16
Євген Євген <epronenko@gmail.com>
2022-01-15 20:24:08 +0200

    try to find person's name

--- src/components/AuthFB.js
+++ src/components/AuthFB.js
@@ -3,15 +3,19 @@ import FacebookLogin from "react-facebook-login";
 
 const AuthFb = () => {
     const [auth, setAuth] = useState(false)
-    const responseFacebook = response => response.name
+    const responseFacebook = response => {
+        console.log(response);
+        return response.name
+    }
 
+    console.log(responseFacebook);
     useEffect(()=>{
         if (responseFacebook) {
             setAuth(true)
         } else  setAuth(false)
     }, [])
 
-    return !auth ? <FacebookLogin
+    return auth ? <FacebookLogin
         appId="1701100586887951"
         autoLoad={true}
         fields="name,email,picture"
